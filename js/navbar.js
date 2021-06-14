37f2b8194c18e5afd477b36245ad71dfce4f271e
JP Gonzalez <43384928+DrJP99@users.noreply.github.com>
2021-06-14 00:43:46 -0500

    Added "dynamic" navbar and footer

---
+++ js/navbar.js
@@ -0,0 +1,41 @@
+function getPath(){
+    var str=window.location.href;
+    var str_array=str.split('/');
+    var construct=false;
+    var res="";
+    str_array.forEach(element => {
+        if(construct ){
+            res+="../";
+            return;
+        }
+        element=element.toLowerCase();
+        if(element=="html" || element=="src"){
+            construct=true;
+        }
+    });
+    return res;
+}
+
+$(document).ready(function(){
+    //file with relative path to index outside html folder
+    $.get(getPath()+"js/navbar.html", function(html_string){
+        $("#navbar").html(html_string);
+        $("#navbar a").each(function() {
+
+            var _href = this.getAttribute("href");
+            if( typeof(_href) != 'undefined' && _href!="" && _href.search(".html")!=-1){
+                $(this).prop("href", getPath()+_href);
+            }
+        });
+    });
+    $.get(getPath()+"js/footer.html", function(html_string){
+        $("#footer-bar").html(html_string);
+        $("#footer-bar a").each(function() {
+
+            var _href = this.getAttribute("href");
+            if( typeof(_href) != 'undefined' && _href!="" && _href.search(".html")!=-1){
+                $(this).prop("href", getPath()+_href);
+            }
+        });
+    });
+});
