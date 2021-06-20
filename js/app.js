b43a72a07e1f9fb5964791d0f0ca30f5c3d2f24c
JP Gonzalez <43384928+DrJP99@users.noreply.github.com>
2021-06-19 20:51:46 -0500

    contact page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-function clipboardCopy() {
+var btnCopy = document.getElementById('copyButton');
+
+btnCopy.addEventListener('click', function() {
 	var copyText = document.getElementById("email");
 
+	copyText.focus();
 	copyText.select();
 	copyText.setSelectionRange(0, 99999);
 
-	document.execCommand("copy");
+	try {
+		let successful = document.execCommand('copy');
+		let msg = successful ? 'successful' : 'unsuccessful';
 
-	console.log(copyText.value);
-};
+		console.log(msg);
+	} catch (e) {
+		console.log('unable to copy');
+	}
+});
