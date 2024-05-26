6de0982a325186c02d6a4aa038bb324392f736c0
DrJP99 <jpgzz1999@gmail.com>
2024-05-26 11:31:36 -0600

    feat: added custom colors to bootstrap theme

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,11 @@
-// import './App.css'
+import { Button } from 'react-bootstrap'
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
+				<h1>Hello</h1>
+				<Button variant="danger">Hello</Button>
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
