35a6e62057b31cd307537dbcb1957ba552a8b629
DrJP99 <jpgzz1999@gmail.com>
2024-05-26 12:08:53 -0600

    fix: navbar links doesnt reload plage

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,20 +1,31 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const MyNavbar = () => {
 	return (
 		<Navbar
 			className="bg-secondary"
-			fixed="top"
+			// fixed="top"
 			collapseOnSelect
 			expand="sm"
 		>
 			<Container>
-				<Navbar.Brand href="/">DrJP99</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">
+					DrJP99
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-na" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="/">Projects</Nav.Link>
-						<Nav.Link href="/">My GitHub</Nav.Link>
+					<Nav className="me-auto justify-content-end">
+						<Nav.Item>
+							<Nav.Link as={Link} to="/projects">
+								Projects
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link as={Link} to="/github">
+								My GitHub
+							</Nav.Link>
+						</Nav.Item>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
